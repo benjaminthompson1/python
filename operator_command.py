fc03055f5c495cf13a277c824a44bdb4795ff45f
Benjamin Thompson <benjamin.thompson1@ibm.com>
2023-08-28 13:03:08 +1000

    wip

--- operator_command.py
+++ operator_command.py
@@ -2,7 +2,7 @@
 import zoautil_py.opercmd as opercmd
 
 # Issue an operator command
-opercmd.execute(command="d", parameters="iplinfo", terse=True)
+response = opercmd.execute(command="d", parameters="iplinfo", terse=True)
 
 # Print the response
 print("Return Code:", response.rc)
