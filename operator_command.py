4bdb162cd92bfeb49d1ca35119d70b230f08f77d
Benjamin Thompson <benjamin.thompson1@ibm.com>
2023-08-28 13:13:01 +1000

    wip

--- operator_command.py
+++ operator_command.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import zoautil_py.opercmd as opercmd
+import zoautil_py.datasets as datasets
 
 # Issue an operator command
 response = opercmd.execute(command="d", parameters="iplinfo", terse=True)
 
 # Print the response
 print("Return Code:", response.rc)
+
+# Print the high-level qualifier
+print("High-Level Qualifier:", datasets.hlq())
